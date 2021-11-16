2a48b3345b4968e80e6dace45cf220974f6e7485
José Augusto <73261477+Jose-augusto-git@users.noreply.github.com>
2021-11-16 17:10:11 -0300

    2.0

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php 
+    require_once "../vendor/autoload.php";
+
+    $route = new \App\Route;
 
     echo 'Conseguiu está funcionando';
+    echo '<hr>';
+
+
+    print_r($route->getUrl());
 
 ?>
